1b4e65ff2a22da2e356db3cbcec7dea2769ac17b
DanielCasquino <daniel.casquino@utec.edu.pe>
2025-09-24 20:31:53 -0500

    wip: pys

---
+++ 3/udp_sender.py
@@ -0,0 +1,32 @@
+import socket
+import zlib
+import struct
+
+#checksum generation function
+def checksum_calculator(data):
+    checksum = zlib.crc32(data)
+    return checksum
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = b"Hello, World!"
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+
+data_length = len(MESSAGE)
+print("Data length: %s" % data_length)
+#checksum = checksum_calculator(MESSAGE)
+checksum = int("4", 16) #forced checksum value
+print("Checksum: %s" % checksum)
+
+#header construction
+udp_header = struct.pack("!IIII", UDP_PORT, UDP_PORT, data_length, checksum)
+print("Header: %s" % udp_header)
+MESSAGE = udp_header + MESSAGE #concatenation
+
+sock = socket.socket(socket.AF_INET,    # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE,(UDP_IP, UDP_PORT))
